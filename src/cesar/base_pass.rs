13c9c8d29336fdad161aa2c29fcdb3634e63aaf1
Ravneet Singh Bhatia <ravneet.bhatia@outlook.com>
2024-07-19 12:27:16 -0700

    implementing get_runner

--- src/cesar/base_pass.rs
+++ src/cesar/base_pass.rs
@@ -4,4 +4,19 @@ use egg::*;
 use log::debug;
 use log::info;
 
+pub static mut ASSUMPTIONS: String = String::new();
 
+pub trait BasePass {
+    fn make_rules() -> Vec<Rewrite<PropLang, ()>>
+
+    fn get_runner(has_node_limit: bool) -> Runner<PropLang, ()> {
+        let mut runner Runner::<PropLang, ()>::default();
+
+        if has_node_limit {
+            runner
+                .with_node_limit(100_000)
+                .with_iter_limit(100_000)
+        } else {
+            runner
+        }
+};
