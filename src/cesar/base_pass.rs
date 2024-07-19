4dbb34c3980f30db904a506305b4d44388dd7992
Ravneet Singh Bhatia <ravneet.bhatia@outlook.com>
2024-07-19 14:06:50 -0700

    implementing simplify.rs

--- src/cesar/base_pass.rs
+++ src/cesar/base_pass.rs
@@ -19,4 +19,31 @@ pub trait BasePass {
         } else {
             runner
         }
+    }
+
+    fn simplify(problem: String, assumptions: String, has_node_limit: bool, timeout: u64) -> String {
+        debug("Running simplify with {0}", has_node_limit);
+        unsafe { ASSUMPTIONS = assumptions };
+        
+        let problem = problem.parse().unwrap();
+        let rules = Self::make_rules();
+
+        let mut runner = Self::get_runner(has_node_limit)
+            .with_time_limit(std::time::Duration::from_secs(timeout))
+            .with_explanations_enabled()
+            .with_expr(&problem).run(&rules);
+
+        let extractor = Extractor::new(&runner.egraph, AstSize);
+        let simplified = extractor.find_best(runner.roots[0]);
+        debug!("Start of Pass debug info");
+        debug!("Simplification finished");
+        debug!("Original Problem: {}", problem);
+        debug!("Simplifed Problem: {}", simplified.1);
+        debug!("Cost of Simplification: {}", simplified.0);
+        debug!("Runner stop reason: {:?}", runner.stop_reason);
+
+        let explanation = runner.explain_equivalence(&problem, &simplified.1).get_flat_string();
+        debug!("Explanations: {}, explanation");
+        debug!("End of Pass debug info");
+
 };
