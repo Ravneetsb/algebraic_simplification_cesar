0cb8b72837bddfe07f29b51d80e6387ad799631e
Ravneet Singh Bhatia <ravneet.bhatia@outlook.com>
2024-07-22 09:33:09 -0700

    adding new timeouts

--- src/cesar/config.rs
+++ src/cesar/config.rs
@@ -1,5 +1,11 @@
 // Set the timeout of all passes in seconds.
 pub static TIMEOUT: u64 = 30;
 
+// Set the short timeout for the passes.
+pub static SHORT_TIMEOUT: u64 = 1;
+
+// Set the long timeout for the passes.
+pub static LONG_TIMEOUT: u64 = 90;
+
 // If true, print debug information.
 pub static DEBUG: bool = false;
